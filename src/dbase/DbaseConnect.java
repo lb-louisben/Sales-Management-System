a5c45161e12b13e5129410c36f65bd67f1f619bc
louisben <louisben201721@gmail.com>
2021-06-02 15:45:47 +0800

    database connection

--- src/dbase/DbaseConnect.java
+++ src/dbase/DbaseConnect.java
@@ -1,5 +1,9 @@
 package dbase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /*-
  * @program: Sales-Management-System
  *
@@ -10,5 +14,23 @@ package dbase;
  * @creat: 2021-06-02-15:26
  * */
 public class DbaseConnect {
+    //    public static Connection conn;
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/dbase";//这里是连接User数据库哦
+    static final String USER = "root";//用户
+    static final String PASSWORD = "Hwb..//0987";//密码
+//    public static Connection conn;
 
+    public static Connection getConn() {
+        Connection conn;
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            System.out.println("Successfully!!!!");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return conn;
+    }
 }
