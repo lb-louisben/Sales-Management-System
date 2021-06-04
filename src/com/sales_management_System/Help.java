dd2e184c2446649d4f09e113c7a2a354b300df43
louisben <louisben201721@gmail.com>
2021-06-04 16:05:51 +0800

    关于

--- src/com/sales_management_System/Help.java
+++ src/com/sales_management_System/Help.java
@@ -30,19 +30,19 @@ public class Help extends JFrame implements ActionListener {
         lbl2.setForeground(new Color(0, 1, 0));
         lbl2.setFont(new Font("宋体", Font.BOLD, 20));
         this.add(lbl2);
-        Label lbl3 = new Label("销售管理，帮助，五大选项，用户管理又分4个小点：删除，修改，查询以及");
+        Label lbl3 = new Label("销售管理，帮助，退出系统六大选项，用户管理又分4个小点：删除，修改，");
         lbl3.setForeground(new Color(0, 1, 0));
         lbl3.setFont(new Font("宋体", Font.BOLD, 20));
         this.add(lbl3);
-        Label lbl4 = new Label("注册，库存管理又分两个小点：查找，并修改库存；进货管理主要是\"对新的");
+        Label lbl4 = new Label("查询以及注册，库存管理又分两个小点：查找，并修改库存；进货管理主要是");
         lbl4.setForeground(new Color(0, 1, 0));
         lbl4.setFont(new Font("宋体", Font.BOLD, 20));
         this.add(lbl4);
-        Label lbl5 = new Label("产品和老的产品的录入和介绍，营销系统针对产品的介绍，并卖掉了多少产品");
+        Label lbl5 = new Label("\"对新的产品和老的产品的录入和介绍，销售管理系统针对产品的介绍，并卖");
         lbl5.setForeground(new Color(0, 1, 0));
         lbl5.setFont(new Font("宋体", Font.BOLD, 20));
         this.add(lbl5);
-        Label lbl6 = new Label("最后的退出系统选择是否保存，并对系统的简单介绍。                 ");
+        Label lbl6 = new Label("掉了多少产品。帮助中的关于是对系统的简单介绍。最后退出系统结束操作。");
         lbl6.setForeground(new Color(0, 1, 0));
         lbl6.setFont(new Font("宋体", Font.BOLD, 20));
         this.add(lbl6);
@@ -52,7 +52,6 @@ public class Help extends JFrame implements ActionListener {
         this.setVisible(true);
         this.setResizable(true);
     }
-
     public void actionPerformed(ActionEvent e) {
     }
 }
